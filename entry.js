2947ca9e3dbad08213b2dd09ffb080e6a6be7ca9
Weilbyte <git@weilbyte.dev>
2020-09-15 02:46:30 +0200

    fuck you node

--- entry.js
+++ entry.js
@@ -24,12 +24,12 @@ const WORKSHOPVDF = `
 fs.writeFileSync('/home/steam/workshop.vdf', WORKSHOPVDF)
 
 if (process.env.STEAM_TFASEED) {
-    child_process.spawn(`/home/steam/steamcmd-2fa -username ${process.env.STEAM_USERNAME} -password ${process.env.STEAM_PASSWORD} -seed ${process.env.STEAM_TFASEED} -args "+workshop_build_item /home/steam/workshop.vdf +quit"`,
+    child_process.spawn('/home/steam/steamcmd-2fa', ['-username', process.env.STEAM_USERNAME, '-password', process.env.STEAM_PASSWORD, '-seed', process.env.STEAM_TFASEED, '-args', "+workshop_build_item /home/steam/workshop.vdf +quit"],
     {
         stdio: 'inherit'
     })
 } else {
-    child_process.spawn('/home/steam/steamcmd/steamcmd.sh', [`+login ${process.env.STEAM_USERNAME} ${process.env.STEAM_PASSWORD} +workshop_build_item /home/steam/workshop.vdf +quit`],
+    child_process.spawn('/home/steam/steamcmd/steamcmd.sh', ['+login', process.env.STEAM_USERNAME, process.env.STEAM_PASSWORD, '+workshop_build_item', '/home/steam/workshop.vdf', '+quit'],
     {
         stdio: 'inherit'
     })
