2515726f448a1068a05f7a10bb59b47978dd4b71
Weilbyte <git@weilbyte.dev>
2020-09-15 18:43:54 +0200

    Fix repo path?

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,11 +14,11 @@ else
   password="${STEAM_PASSWORD}"
 fi
 
-cat << EOF > /workshop.vdf
+cat << EOF > /home/steam/workshop.vdf
 "workshopitem"
 {
   "appid" "${INPUT_APPID}"
-  "contentfolder" "${repo}/${INPUT_PATH}"
+  "contentfolder" "${GITHUB_WORKSPACE}/${INPUT_PATH}"
   "changenote" "${INPUT_CHANGENOTE}"
   "publishedfileid" "${INPUT_ITEMID}"
 }
@@ -27,10 +27,13 @@ EOF
 echo "$(cat /workshop.vdf)"
 
 if [[ -z "${STEAM_TFASEED}" ]]; then
-  /home/steam/steamcmd/steamcmd.sh +@ShutdownOnFailedCommand 1 +login ${STEAM_USERNAME} ${STEAM_PASSWORD} +workshop_build_item /workshop.vdf +quit
+  /home/steam/steamcmd/steamcmd.sh +@ShutdownOnFailedCommand 1 +login ${STEAM_USERNAME} ${STEAM_PASSWORD} +workshop_build_item /home/steam/workshop.vdf +quit
 else
   code=$(/home/steam/steamcmd-2fa --username ${STEAM_USERNAME} --password ${STEAM_PASSWORD} --seed ${STEAM_TFASEED} --code-only)
-  /home/steam/steamcmd/steamcmd.sh +@ShutdownOnFailedCommand 1 +login ${STEAM_USERNAME} ${STEAM_PASSWORD} $code +workshop_build_item /workshop.vdf +quit
+  if [[ $? -ne 0 ]]; then
+    exit 1 
+  fi
+  /home/steam/steamcmd/steamcmd.sh +@ShutdownOnFailedCommand 1 +login ${STEAM_USERNAME} ${STEAM_PASSWORD} $code +workshop_build_item /home/steam/workshop.vdf +quit
 fi
 
 [ $? -eq 0 ] && exit 0 || (
