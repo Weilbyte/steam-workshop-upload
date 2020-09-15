54a286072a6eb8f0529a055c4640e168cad04094
Weilbyte <git@weilbyte.dev>
2020-09-15 03:46:46 +0200

    use spawnSync

--- entry.js
+++ entry.js
@@ -25,7 +25,7 @@ fs.writeFileSync('/home/steam/workshop.vdf', WORKSHOPVDF)
 
 if (process.env.STEAM_TFASEED) {
     let args = `-username ${process.env.STEAM_USERNAME} -password ${process.env.STEAM_PASSWORD} -seed ${process.env.STEAM_TFASEED} -args "+workshop_build_item /home/steam/workshop.vdf +quit"`
-    child_process.spawn('/home/steam/steamcmd-2fa', args.split(" "),
+    child_process.spawnSync('/home/steam/steamcmd-2fa', args.split(" "),
     {
         stdio: 'inherit',
         timeout: 60000,
@@ -33,7 +33,7 @@ if (process.env.STEAM_TFASEED) {
     })
 } else {
     let args = `+login ${process.env.STEAM_USERNAME} ${process.env.STEAM_PASSWORD} +workshop_build_item /home/steam/workshop.vdf +quit`
-    child_process.spawn('/home/steam/steamcmd/steamcmd.sh', args.split(" "),
+    child_process.spawnSync('/home/steam/steamcmd/steamcmd.sh', args.split(" "),
     {
         stdio: 'inherit',
         timeout: 60000,
